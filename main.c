4e4d0f15667992f51f0b69b23e4502dd1265e951
Pablo Ramirez <pablo_ramirez@ucol.mx>
2015-11-03 10:41:35 -0600

    update main, try read file

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <curses.h>
+
+FILE *fr;
 
 int main(){
 	iniciar();
@@ -7,5 +10,31 @@ int main(){
 	float b[3] = {2.4, 2.9, 4.1};
 	init_nbody(3, a, b);
 	compute_potential();
+
+	char line[256];
+	char header[256];	
+	fr = fopen("32Mpc_050.0256.fvol", "rt");
+	while(fgets(line, 256, fr) != NULL){
+		sscanf(line, "%s", header);		
+	}
+
+	fclose(fr);
+
+	printf("%s\n", header);
+
+	
+
 	return 0;
 }
+
+/* 
+	char buff[256];
+	fr = fopen("32Mpc_050.0256.fvol", "r");
+	fscanf(fr, "%s", buff);
+	printf("%s\n", buff);
+
+	fgets(buff, 256, (FILE*)fr);
+	printf("%s\n", buff);
+
+	fclose(fr);
+*/
